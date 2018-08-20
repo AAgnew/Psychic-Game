da4d836fd2feabfd5644400e0e9e3262d30e1db2
Alicia Agnew <alicia.agnew@gmail.com>
2018-08-19 20:26:05 -0400

    Updating project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,20 @@
 //Solutions
-var womenIntech = [
+var womenInTech = [
     "Ellen Pao",
     "Grace Hopper",
     "Hedy Lamarr",
     "Radia Joy Perlman",
 ]
 
-var userGuesses=[]
+// using above array we choose a random word.
+var Solution = womenInTech[Math.floor(Math.random() * womenInTech.length)];
+console.log(womenInTech)
+
+//User Answers
+var userGuess = []
+//TotalWins
+var wins = 0
+//TotalAttempts 
+var maxWrong = 9
+
+//Let the game begin 
